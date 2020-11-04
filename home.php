808da96792aa4754eadd93b1ed4aff113e70f8d5
Narsingh kumar chauhan <50392800+narsi2510@users.noreply.github.com>
2020-11-04 17:45:26 +0530

    Add files via upload
    This website is based on Photography  management system . Here admin can upload their creative photos and user can easily download .

---
+++ home.php
@@ -0,0 +1,35 @@
+<?php
+// We need to use session
+session_start();
+// If user has not logged in and tries to acess then
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit;
+}
+// html below will only work if user has logged in
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Home Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Website Title</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Home Page</h2>
+			<p>Welcome back, <?=$_SESSION['name']?>!</p>
+		</div>
+	</body>
+</html>
